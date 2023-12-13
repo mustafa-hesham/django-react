e09e01ba33178c21f0a09e2bec39edfc27735a1a
Mustafa Hesham <mustafa.hesham@scandiweb.com>
2023-12-14 00:39:29 +0200

    Autofixes and installed sass

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,3 +1,2 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-const { useBabelRc, override } = require("customize-cra");
+const { useBabelRc, override } = require('customize-cra');
 module.exports = override(useBabelRc());
