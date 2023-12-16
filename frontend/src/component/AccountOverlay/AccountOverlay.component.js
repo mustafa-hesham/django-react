fba47be3d60bf80e316d940c6678cfeca1226da4
Mustafa Hesham <mustafa.hesham@scandiweb.com>
2023-12-16 10:10:19 +0200

    Created login form

--- frontend/src/component/AccountOverlay/AccountOverlay.component.js
+++ frontend/src/component/AccountOverlay/AccountOverlay.component.js
@@ -10,6 +10,37 @@ class AccountOverlayComponent extends PureComponent {
     overlayRef: RefType.isRequired,
   };
 
+  renderTitle() {
+    return (
+      <div className='AccountOverlay-Title'>
+        <h2>Login</h2>
+        <h2>Create account</h2>
+      </div>
+    );
+  }
+
+  renderLogin() {
+    return (
+      <div className='AccountOverlay-Login'>
+        <form className='AccountOverlay-LoginForm'>
+          <input
+            type="text"
+            name="username"
+            placeholder='Username'
+            className='AccountOverlay-LoginTextField'
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder='Password'
+            className='AccountOverlay-LoginTextField'
+          />
+          <input type="submit" value="Login" className='AccountOverlay-LoginButton'/>
+        </form>
+      </div>
+    );
+  }
+
   render() {
     const {
       isOverlayToggled,
@@ -23,6 +54,8 @@ class AccountOverlayComponent extends PureComponent {
         className={ className }
         ref={ overlayRef }
       >
+        { this.renderTitle() }
+        { this.renderLogin() }
       </div>
     );
   }
