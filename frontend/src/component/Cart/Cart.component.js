ac9a218299c81940f9673a4002d5746c771afd73
Mustafa Hesham <mustafa.hesham@scandiweb.com>
2023-12-30 12:44:33 +0200

    Modified cart component

--- frontend/src/component/Cart/Cart.component.js
+++ frontend/src/component/Cart/Cart.component.js
@@ -2,17 +2,23 @@ import './Cart.style.scss';
 
 import Overlay from 'Component/Overlay';
 import { useRef } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { updateToggleCartOverlay } from 'Store/Cart/CartReducer.reducer';
 import CartIcon from 'Style/icons/Cart/cart-icon-60.png';
 
 export default function Cart() {
   const isCartOverlayToggled = useSelector((state) => state.CartReducer.isCartOverlayToggled);
   const cartIconRef = useRef(null);
-  console.log(cartIconRef );
+  const dispatch = useDispatch();
+
   return (
     <div className='Cart'>
-      <img className='Cart-CartIcon' src={ CartIcon } ref={ cartIconRef }/>
+      <img
+        className='Cart-CartIcon'
+        src={ CartIcon }
+        ref={ cartIconRef }
+        onClick={ () => dispatch(updateToggleCartOverlay(!isCartOverlayToggled)) }
+      />
       { cartOverlay(cartIconRef, isCartOverlayToggled) }
     </div>
   );
