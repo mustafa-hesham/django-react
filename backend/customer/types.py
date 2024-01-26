0c2f11bdf8118638fe116045ca70fdb90be3b672
Mustafa Hesham <mustafa.hesham@scandiweb.com>
2024-01-26 20:40:09 +0200

    Added create account form validation

--- backend/customer/types.py
+++ backend/customer/types.py
@@ -1,3 +1,4 @@
+import graphene
 from graphene_django import DjangoObjectType
 from django.contrib.auth import get_user_model
 
@@ -6,3 +7,12 @@ class CustomerType(DjangoObjectType):
     class Meta:
         model = get_user_model()
         exclude = ("password",)
+
+
+class CustomerTypeInput(graphene.InputObjectType):
+    email = graphene.String()
+    password = graphene.String()
+    password2 = graphene.String()
+    firstName = graphene.String()
+    lastName = graphene.String()
+    birthDate = graphene.String()
--- frontend/src/component/AccountCreateAccount/AccountCreateAccount.component.js
+++ frontend/src/component/AccountCreateAccount/AccountCreateAccount.component.js
@@ -5,26 +5,65 @@ import { useState } from 'react';
 
 export default function AccountCreateAccount() {
   const [startDate, setStartDate] = useState(new Date());
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+
+  const [input, setInput] = useState({
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const [error, setError] = useState({
+    email: '',
+    password: '',
+    password2: ''
+  });
+
   return (
     <div className='AccountCreateAccount'>
       <form
         className='AccountCreateAccount-CreateAccountForm OverlayForm'
-        onSubmit={ (e) => handleCreateAccount(e) }
+        onSubmit={ (e) => handleCreateAccount(e, isFormSubmitted, error) }
       >
-        <input
-          type="email"
-          name="email"
-          placeholder='Email'
-          className='AccountCreateAccount-TextField OverlayForm-TextField'
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder='Password'
-          className='AccountCreateAccount-TextField OverlayForm-TextField'
-          required
-        />
+        <div className='AccountCreateAccount-TextFieldWrapper OverlayForm-TextFieldWrapper'>
+          <input
+            type="email"
+            name="email"
+            placeholder='Email'
+            value={ input.email }
+            onChange={ (e) => onInputChange(e, input, error, setError, setInput) }
+            className='AccountCreateAccount-TextField OverlayForm-TextField'
+            required
+          />
+          { isFormSubmitted &&
+        error.email && <div className='AccountCreateAccount-Error OverlayForm-Error'>{ error.email }</div> }
+        </div>
+        <div className='AccountCreateAccount-TextFieldWrapper OverlayForm-TextFieldWrapper'>
+          <input
+            type="password"
+            name="password"
+            placeholder='Password'
+            value={ input.password }
+            onChange={ (e) => onInputChange(e, input, error, setError, setInput) }
+            className='AccountCreateAccount-TextField OverlayForm-TextField'
+            required
+          />
+          { isFormSubmitted &&
+        error.password && <div className='AccountCreateAccount-Error OverlayForm-Error'>{ error.password }</div> }
+        </div>
+        <div className='AccountCreateAccount-TextFieldWrapper OverlayForm-TextFieldWrapper'>
+          <input
+            type="password"
+            name="password2"
+            placeholder='Confirm password'
+            value={ input.password2 }
+            onChange={ (e) => onInputChange(e, input, error, setError, setInput) }
+            className='AccountCreateAccount-TextField OverlayForm-TextField'
+            required
+          />
+          { isFormSubmitted &&
+        error.password2 && <div className='AccountCreateAccount-Error OverlayForm-Error'>{ error.password2 }</div> }
+        </div>
         <input
           type="text"
           name="firstName"
@@ -47,13 +86,14 @@ export default function AccountCreateAccount() {
           type="submit"
           value="Create Account"
           className='AccountCreateAccount-CreateAccountButton OverlayForm-SubmitButton'
+          onClick={ () => setIsFormSubmitted(true) }
         />
       </form>
     </div>
   );
 };
 
-async function handleCreateAccount(event) {
+async function handleCreateAccount(event, isFormSubmitted, error) {
   event.preventDefault();
   const {
     target: {
@@ -68,6 +108,55 @@ async function handleCreateAccount(event) {
       }
     }
   } = event;
-
+  console.log(isFormSubmitted && !!Object.values(error).find((value) => !!value));
   console.log(emailValue, passwordValue, datePickerValue);
+};
+
+function validateInput(e, input, error, setError) {
+  const { name, value } = e.target;
+  setError((prev) => {
+    const stateObj = { ...prev, [name]: '' };
+
+    switch (name) {
+      case 'email':
+        if (!value) {
+          stateObj[name] = 'Please enter email.';
+        } else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
+          stateObj[name] = 'Please enter a valid email.';
+        }
+        break;
+
+      case 'password':
+        if (!value) {
+          stateObj[name] = 'Please enter Password.';
+        } else if (input.password2 && value !== input.password2) {
+          stateObj['password2'] = 'Password and confirm password don\'t match.';
+        } else {
+          stateObj['password2'] = input.password2 ? '' : error.password2;
+        }
+        break;
+
+      case 'password2':
+        if (!value) {
+          stateObj[name] = 'Please enter Confirm Password.';
+        } else if (input.password && value !== input.password) {
+          stateObj[name] = 'Password and confirm password don\'t match.';
+        }
+        break;
+
+      default:
+        break;
+    }
+
+    return stateObj;
+  });
+};
+
+function onInputChange(e, input, error, setError, setInput) {
+  const { name, value } = e.target;
+  setInput((prev) => ({
+    ...prev,
+    [name]: value
+  }));
+  validateInput(e, input, error, setError);
 };
