eefbe3e695162e929d3e85e3eeb9b53987e9b5eb
Mustafa Hesham <mustafa.hesham@scandiweb.com>
2024-01-31 20:35:44 +0200

    Displayed guest favorites when customer is not signed in

--- frontend/src/route/FavoritesPage/FavoritesPage.component.js
+++ frontend/src/route/FavoritesPage/FavoritesPage.component.js
@@ -3,15 +3,19 @@ import './FavoritesPage.styles.scss';
 import Header from 'Component/Header';
 import ProductCard from 'Component/ProductCard';
 import { useSelector } from 'react-redux';
+import { getGuestFavorites } from 'Util/Favorites';
 
 export default function FavoritesPage() {
   const customerFavorites = useSelector((state) => state.CustomerReducer.customer.favorites);
+  const currentFavorites = Array.isArray(customerFavorites) && customerFavorites.length ?
+    customerFavorites :
+    getGuestFavorites();
 
   return (
     <div className='FavoritesPage'>
       <Header />
       <div className='FavoritesPage-Body'>
-        { renderProductListGrid(customerFavorites) }
+        { renderProductListGrid(currentFavorites) }
       </div>
     </div>
   );
