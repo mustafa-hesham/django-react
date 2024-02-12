17ee8dd4478767a71d6de80909533a247af60918
Mustafa Hesham <mustafa.hesham@scandiweb.com>
2024-02-12 14:46:32 +0200

    Moved logout to the account overlay

--- frontend/src/component/Account/Account.component.js
+++ frontend/src/component/Account/Account.component.js
@@ -4,11 +4,9 @@ import AccountOverlay from 'Component/AccountOverlay';
 import { useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateToggleAccountOverlay } from 'Store/AccountOverlay/AccountOverlayReducer.reducer';
-import { logOut } from 'Util/Customer';
 
 import {
-  LOGIN,
-  LOGOUT
+  LOGIN
 } from './Account.config';
 
 export default function Account() {
@@ -27,36 +25,10 @@ export default function Account() {
       >
         { customerName ? customerName : LOGIN }
       </div>
-      { renderLogOut() }
       <AccountOverlay
         isOverlayToggled={ isOverlayToggled }
         accountRef ={ accountRef }
       />
     </div>
   );
-};
-
-function renderLogOut() {
-  const customerName= useSelector((state) => state.CustomerReducer.customer.email);
-
-  if (!customerName) {
-    return null;
-  }
-
-  return (
-    <div
-      className='Account-Logout Header-Text'
-    >
-      <div
-        role='Button'
-        onClick={ logoutCustomer }
-      > { LOGOUT }
-      </div>
-    </div>
-  );
-};
-
-function logoutCustomer() {
-  logOut();
-  location.reload();
 };
